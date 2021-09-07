bb384b151f2bd2ffd2e70b470e04d37efc1cbdca
lucy922 <ofemlucy@gmail.com>
2021-09-07 08:11:03 +0100

    modified component

--- src/components/Post.js
+++ src/components/Post.js
@@ -15,10 +15,12 @@ const Post = ({ post, onDelete }) => {
        <div className="post-content">
         <div className="post-details">
           <div className="ps">
-            <div className="post-link">
+            <div>
               <a href="/">
                 <span className="post-name">{post.name}</span>
-                 <FaCheckCircle/>
+                <span className="post-link">
+                <FaCheckCircle/>
+                </span>
                 <span className="post-username">{post.username}</span>
                 <span className="post-time">{post.time}</span>
               </a>
